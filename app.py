fbc360aa57bc7dbd25c1396d86ca05004f26b993
ronald-abreu <ronaldnascimento2@hotmail.com>
2025-07-06 21:10:30 -0300

    fix: add fallback for DATABASE_URL to use SQLite in CI

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flasgger import Swagger
 from os import environ
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DATABASE_URL')
+app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DATABASE_URL', 'sqlite:///:memory:')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
